f584b83c9eef9a1266fc817fef04716f516b0f3f
ApurvaHadke <apurvah76@gmail.comm>
2024-12-29 20:05:14 +0530

    This is first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//let start coding here:
+
+let arr = [1,2,4,5,8,15,25,90];
+let count = 0;
+for(let i=0;i<=arr.length;i++){
+   if(i%2==1){
+      count++
+   }
+}
+console.log('count of odd number ${count}')
